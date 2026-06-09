138c64e8e8ef4622b246e1e76eaa78f1b90e6cd2
Jaissy <vjaissy@gmail.com>
2026-06-09 20:26:02 +0530

    Connect frontend to Render backend

--- artifacts/wander-india/src/main.tsx
+++ artifacts/wander-india/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "@workspace/api-client-react/custom-fetch";
+
+setBaseUrl("https://wander-india.onrender.com");
+
 createRoot(document.getElementById("root")!).render(<App />);
